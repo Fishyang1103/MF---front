074144234b5065443923dd747e6a07112edf7d83
Fishyang1103 <49112216+Fishyang1103@users.noreply.github.com>
2022-04-20 01:33:45 +0800

    Update About

--- src/views/front/About.vue
+++ src/views/front/About.vue
@@ -8,16 +8,16 @@
         ).
           花#[br]
           在時間與空間中傳遞情感#[br]
-          拉近人與人之間的距離#[br]
-          送與收的人總相視而笑#[br]#[br]
-          取自兩個朋友英文名字開頭#[br]
-          然而這也是 M 的一個夢想#[br]
-          之所以會使用玫瑰花當 logo#[br]
-          因為允諾送玫瑰花的前一天#[br]
-          Ｍ 去天堂了#[br]
-          所以始終欠了一束玫瑰花#[br]
-          在未來某一世相遇的時候#[br]
-          願能一同完成這輩子的夢#[br]
+          也不只是特殊節日才出現#[br]
+          從商品轉換成人與人互動的一種媒介#[br]
+          讓送與收的人相視而笑#[br]
+          店名取自與好友的英文名開頭#[br]
+          緊密而保有彼此#[br]
+          然而乘載了夢想跟遺願#[br]
+          留下的人希望這的一隅#[br]
+          能偶爾想念#[br]
+          並帶著回憶走完剩下的人生#[br]
+          只要記得就永遠不會消失#[br]
     b-row.flex-row-reverse.mt-5
       b-col(sm='12', md='6').p-0
         iframe.ml-5(src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.7036497990503!2d121.41732691433089!3d25.04412928396764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a7bec9ad74b1%3A0xa639904a89f26435!2z5Yue5YuV6YOo5Yue5YuV5Yqb55m85bGV572y5YyX5Z-65a6c6Iqx6YeR6aas5YiG572y5rOw5bGx6IG35qWt6KiT57e05aC0!5e0!3m2!1szh-TW!2stw!4v1640161938195!5m2!1szh-TW!2stw' width='517' height='400' style='border:0;' allowfullscreen loading='lazy')
